6eef5bba3174977abc605042cd3d2fc3500d5235
diego <diegomota2374@gmail.com>
2025-12-29 15:05:41 -0300

    Update favicon, enhance Vite configuration with path aliases, and introduce new components for interactive avatar and button. Refactor CSS for improved styling and responsiveness, and add utility classes for better design consistency. Integrate translations for interaction hints and update hero section with animated elements.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
 })
 
